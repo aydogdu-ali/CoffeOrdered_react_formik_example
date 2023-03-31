fc9aff8d337376c50e8cdc7f58e4d0d414aa0281
Ali <ali.ozgur@example.com>
2023-03-31 14:24:48 +0300

    edit register components

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useFormik } from "formik";
+import React from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  // formik yapısının içinde kullanacağımız yapılaıı çıkartarak kullanabiliriz.
+  // const formik = useFormik({
+  const { values, errors, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      email: "",
+      age: "",
+      password: "",
+      repeatPassword: "",
+    },
+  });
 
-export default Register
+  console.log(values);
+
+  return (
+   
+      <form className="register">
+        <div className="form-input">
+          <label> Email</label>
+          <input
+            type="email"
+            value={values.email}
+            onChange={handleChange}
+            id="email"
+            placeholder="lütfen mail adresinizi giriniz!"
+          />
+        </div>
+        <div className="form-input">
+          <label> Yaş</label>
+          <input
+            type="number"
+            value={values.age}
+            onChange={handleChange}
+            id="age"
+            placeholder="lütfen yaşınızı  giriniz!"
+          />
+        </div>
+        <div className="form-input">
+          <label> Şifre</label>
+          <input
+            type="password"
+            value={values.password}
+            onChange={handleChange}
+            id="password"
+            placeholder="lütfen şifrenizi giriniz!"
+          />
+        </div>
+        <div className="form-input">
+          <label> Şifrenizi Tekrar Giriniz</label>
+          <input
+            type="password"
+            value={values.repeatPassword}
+            onChange={handleChange}
+            id="repeatPassword"
+            placeholder="lütfen şifrenizi tekrar giriniz!"
+          />
+        </div>
+        <button>Kaydet</button>
+      </form>
+   
+  );
+};
+
+export default Register;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from '../components/Home';
+import Register from '../components/Register';
+import Login from '../components/Login';
+
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRouter
